289dbe20f2a35a4607c584e675df94b04812ff06
proprathamesh <prathameshsyadav1712@gmail.com>
2024-10-26 12:39:22 +0530

    cred

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://tejas07:Tejas123@cruddb.cluster-cr6kmqiw62yn.ap-south-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+
+const username = 'tejas07';
+const password = 'Tejas123';
+const dbName = 'crud';  // Replace with your database name
+const clusterEndpoint = 'cruddb.cluster-cr6kmqiw62yn.ap-south-1.docdb.amazonaws.com:27017';
+const caFilePath = '/home/ubuntu/crud-app-mern/Backend/global-bundle.pem';  // Ensure the CA file path is correct
+const url = `mongodb://${username}:${password}@${clusterEndpoint}/${dbName}?tls=true&tlsCAFile=${caFilePath}`;
 
 const connectToMongo = async () => {
   try {
     mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI);
+    mongoose.connect(url);
     console.log("Connected to Mongo Successfully!");
   } catch (error) {
     console.log(error);
