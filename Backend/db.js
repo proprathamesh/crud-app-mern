4b5a6fb8058340dc29a12d9eae6cac22a08e4cb5
proprathamesh <prathameshsyadav1712@gmail.com>
2024-10-26 12:10:13 +0530

    cred

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = "mongodb://tejas07:<insertYourPassword>@cruddb.cluster-cr6kmqiw62yn.ap-south-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+const mongoURI = "mongodb://tejas07:Tejas123@cruddb.cluster-cr6kmqiw62yn.ap-south-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
 
 const connectToMongo = async () => {
   try {
