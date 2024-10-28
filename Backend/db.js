2ea85de4afb2477cbe35e9f88d3f98dfa80570a1
proprathamesh <prathameshsyadav1712@gmail.com>
2024-10-28 08:00:10 +0530

    mongoURL change

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,7 @@ const password = 'Tejas123';
 const dbName = 'crud';  // Replace with your database name
 const clusterEndpoint = 'cruddb.cluster-cr6kmqiw62yn.ap-south-1.docdb.amazonaws.com:27017';
 const caFilePath = '/home/ubuntu/crud-app-mern/Backend/global-bundle.pem';  // Ensure the CA file path is correct
-const url = `mongodb://${username}:${password}@${clusterEndpoint}/${dbName}?tls=true&tlsCAFile=${caFilePath}`;
+const url = `mongodb://${username}:${password}@${clusterEndpoint}/${dbName}?tls=true&tlsCAFile=${caFilePath}&retryWrites=false`;
 
 const connectToMongo = async () => {
   try {
